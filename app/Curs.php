f10c45c3a38134cf2668b87050a19317c069f144
Brian <briihd@gmail.com>
2016-05-17 20:21:31 +0200

    Changed fillable property, missing teacher_id

--- app/Curs.php
+++ app/Curs.php
@@ -9,21 +9,35 @@ class Curs extends Model
     /*
      * Atributs que es poden emplenar "manualment".
      */
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'teacher_id'];
 
     /*
      * Taula on correspon el model
      */
     protected $table = 'cursos';
 
+    public $timestamps = false;
+
     /*
-     * Definir la relació
+     * Definir la relació amb els usuaris
      */
-    public function members()
-    {
+    public function members(){
         return $this->belongsToMany('App\User');
     }
+    /*
+     * Definir la relació amb les tasques
+     */
+    public function tasks(){
+        return $this->hasMany(Task::class);
+    }
+
+    /*
+     * Retorna el nom del professor del curs
+     */
     public function teacher(){
 
+        $teacher = User::find($this->teacher_id);
+        return $teacher->name;
     }
+
 }
