78332aded8b4e6116aef15d0d3153d2592b90f14
Brian <briihd@gmail.com>
2016-05-25 20:26:35 +0200

    Added return button, entregables link, and download the file user has uploaded

--- resources/views/cursos/showUFTask.blade.php
+++ resources/views/cursos/showUFTask.blade.php
@@ -11,6 +11,12 @@
                     <h1>{{$curs->name}}</h1>
                     <h2>{{$uf->name}} {{$uf->description}}</h2>
 
+                    <a href="{{route('cursos.show', array($curs->id))}}">
+                        <button type="submit" class="btn btn-primary">
+                            <i class="fa fa-btn fa-arrow-left"></i>Tornar al curs
+                        </button>
+                    </a>
+
                 </div>
                 <div class="panel-body">
                     <h2>{{$tasca->name}}</h2>
@@ -25,6 +31,13 @@
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h2>Entrega de la tasca</h2>
+                        @if(Auth::user()->isAdmin() || Auth::user()->isTeacher())
+
+                            <a href="{{route('cursos.task.entregables', array($curs->id, $uf->id, $tasca->id))}}">
+                                ENTREGABLES
+                            </a>
+
+                        @endif
                     </div>
                     <div class="panel-body">
                         La tasca finalitza el {{date('d/m/y', strtotime($tasca->data_finalització))}}
@@ -44,20 +57,27 @@
                         @endif
 
                         <div class="entregat">
-                            @if(!$entregable[0]->corregit)
-
-                                <p>La tasca no ha estat corregida encara.</p>
-
+                            @if(!$entregat)
+                                <p>La tasca no ha estat entregada encara.</p>
                             @else
+                                <a href="http://localhost/tasques/{{$curs->id}}/{{$uf->id}}/{{$entregable[0]->file}}"
+                                   download="{{$entregable[0]->file}}">
+                                    Fitxer entregat
+                                </a>
 
-                                <h2>Informació de la correcció:</h2>
+                                @if(!$entregable[0]->corregit)
 
-                                <h3>Nota: </h3><p>$entregable[0]->nota</p>
-                                <h3>Correcció: </h3><p>$entregable[0]->explicacio</p>
+                                <p>La tasca ha sigut entregada però no corregida.</p>
+
+                                @else
+
+                                <h3>Informació de la correcció de l'alumne {{Auth::user()->name}}:</h3>
+                                <p style="font-weight: bold; font-size: 16px">Nota: {{$entregable[0]->nota}}/10</p>
+                                <h4>Feedback: </h4><p>{{$entregable[0]->explicacio}}</p>
+
+                                @endif
                             @endif
-
                         </div>
-
                     </div>
                 </div>
             </div>
