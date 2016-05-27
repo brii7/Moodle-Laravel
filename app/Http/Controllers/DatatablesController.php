f741e44b09b9f454c5d991ac0b0e0cff2a5202e6
Brian <briihd@gmail.com>
2016-05-27 19:39:27 +0200

    Datatables

---
+++ app/Http/Controllers/DatatablesController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Datatables;
+use App\Http\Requests;
+use App\User;
+
+class DatatablesController extends Controller
+{
+    /**
+     * Displays datatables front end view
+     *
+     * @return \Illuminate\View\View
+     */
+    public function getIndex()
+    {
+        return view('admin.allusers');
+    }
+
+    /**
+     * Process datatables ajax request.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function anyData()
+    {
+        return Datatables::of(User::query())->make(true);
+    }
+}
